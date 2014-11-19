4aa3fba68c9323d3d48a1201d42e5e3f4f879ec1
grayyeargin <grayyeargin@gmail.com>
2014-11-19 15:18:11 -0500

    Establishes connection with Eventful API

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,21 @@
 class WelcomeController < ApplicationController
+
   def index
+    @api_response = HTTParty.get('http://api.eventful.com/rest/events/search?app_key=7vwHmZm5RvCdSDtN&location=New+York&q=music')
+
+    @all_events = api_response["search"]["events"]["event"].map do |event|
+      if event["image"] != nil && event["performers"] != nil
+        {event: event["title"], performer: event["performers"]["performer"]["name"], image: event["image"]["url"], venue: event["venue_name"], time: event["start_time"]}
+      else
+        nil
+      end
+    end
+
+    @events = @all_events.compact
+
   end
 end
+
+
+
+
