10a99e84a749619d69647a19c9c9e15580ab7256
grayyeargin <grayyeargin@gmail.com>
2014-11-20 11:45:55 -0500

    Pull in Artist and Album information from spotify API in Artist controller

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -2,8 +2,14 @@ class ArtistController < ApplicationController
 
 	def index
 		query = params[:query]
+		artist_api_response = HTTParty.get("https://api.spotify.com/v1/search?q=#{query}&type=artist")
+    @artist_image = artist_api_response["artists"]["items"][0]["images"][0]["url"]
+    artist_id = artist_api_response["artists"]["items"][0]["id"]
 
-		@api_response = HTTParty.get("https://api.spotify.com/v1/search?q=#{query}&type=artist&r=json")
+    album_api_response = HTTParty.get("https://api.spotify.com/v1/artists/#{artist_id}/albums")
+    @album_image = album_api_response["items"][0]["images"][0]["url"]
 
 	end
 end
+
+
