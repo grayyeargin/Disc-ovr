f35cf16c8b58d3af44d940f39c5a3bcaeb911c3c
grayyeargin <grayyeargin@gmail.com>
2014-11-19 19:15:53 -0500

    Added performer to Eventful API information

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
-    api_response = HTTParty.get('http://api.eventful.com/rest/events/search?app_key=7vwHmZm5RvCdSDtN&location=New+York&q=music')
+    api_response = HTTParty.get('http://api.eventful.com/rest/events/search?app_key=7vwHmZm5RvCdSDtN&location=New+York&q=music&c=concert&page_size=20')
 
     @all_events = api_response["search"]["events"]["event"].map do |event|
-      if event["image"] != nil && event["performers"] != nil
+      if !event["image"].nil? && !event["performers"].nil? && !event["performers"]["performer"].is_a?(Array) && !event["venue_name"].nil? && !event["start_time"].nil?
         {title: event["title"], performer: event["performers"]["performer"]["name"], image: event["image"]["medium"]["url"], venue: event["venue_name"], time: event["start_time"]}
       else
         nil
@@ -15,7 +15,4 @@ class WelcomeController < ApplicationController
 
   end
 end
-
-
-
 
