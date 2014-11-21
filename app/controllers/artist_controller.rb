93e6ca702c76c66d389fba038462a96a785112ed
grayyeargin <grayyeargin@gmail.com>
2014-11-21 12:50:40 -0500

    Fix bugs in artist controller spotify api and artist landing page

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -10,15 +10,19 @@ class ArtistController < ApplicationController
 		artist_id = artist_api_response["artists"]["items"][0]["id"]
 
 		# ////////// ALBUM IMAGES AND PLAY BUTTON //////////
+		# album_api_response = HTTParty.get("https://api.spotify.com/v1/artists/#{artist_id}/albums")
+		# albums_sorted = album_api_response["items"].map do |album|
+		# 	{ album_image: album["images"][0]["url"],
+		# 		album_id: album["id"] }
+		# end
+
 		album_api_response = HTTParty.get("https://api.spotify.com/v1/artists/#{artist_id}/albums")
-		album_sorted = album_api_response["items"].map do |album|
-			{ album_image: album["images"][0]["url"],
-				album_id: album["id"] }
-			end
-		end
-		albums_sorted_unique = albums_sorted.uniq do |album|
-			album[:album_image]
-		end
+		@album_image_0 = album_api_response["items"][0]["images"][0]["url"]
+		@album_id_0 =  album_api_response["items"][0]["id"]
+
+		# albums_sorted_unique = albums_sorted.uniq do |album|
+		# 	album[:album_image]
+		# end
 
 		# ////////// ARTIST BIO //////////
 		last_fm_artist = HTTParty.get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=#{query}&api_key=0689c53570fb3e20176681c7b9d7aa30")
