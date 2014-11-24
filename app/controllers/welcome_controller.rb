0ce3ecb76a8659c86a75639fe4d74e2a48c22129
aarondelaplane <aarondelaplane@icloud.com>
2014-11-24 15:41:29 -0500

    Non-user navbar on one line.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,3 @@
 class WelcomeController < ApplicationController
-
-	@nature = [
-		"http://malsup.github.io/images/p1.jpg",
-		"http://malsup.github.io/images/p2.jpg",
-		"http://malsup.github.io/images/p3.jpg",
-		"http://malsup.github.io/images/p4.jpg"
-	]
 end
 
