07b155d3f5535cbd86684980c2cfe05fa8909b18
isoolio110 <kim.isul@gmail.com>
2014-11-24 20:49:04 -0500

    Adds validation.js file

---
+++ app/assets/javascripts/validation.js
@@ -0,0 +1,73 @@
+$(function(){
+
+	var $firstNameInput = $('#first-name');
+	var $firstNameError = $('#first-name-error');
+	var $lastNameInput = $('#last-name');
+	var $lastNameError = $('#last-name-error');
+	var $userNameInput = $('#username');
+	var $userNameError = $('#username-error');
+
+	$firstNameInput.on("keyup", function(e){
+		var name = $(this).val();
+		if (isValidFirstName(name)){
+			$firstNameError.css('display', 'none');
+		} else {
+			$firstNameError.css('display', 'block');
+		}
+	})
+
+	$lastNameInput.on("keyup", function(e){
+		var lastname = $(this).val();
+		if (isValidLastName(lastname)){
+			$lastNameError.css('display', 'none');
+		} else {
+			$lastNameError.css('display', 'block');
+		}
+	})
+
+	$userNameInput.on("keyup", function(e){
+		var username = $(this).val();
+		if (isValidUserName(username)){
+			$userNameError.css('display', 'none');
+		} else {
+			$userNameError.css('display', 'block');
+		}
+	})
+
+})
+
+
+
+function validFirstName(){
+}
+
+function isValidFirstName(name){
+	var nameRegex = /^[-a-z]+$/;
+	if(nameRegex.test(name)){
+		return true;
+	} else {
+		return false;
+	}
+};
+
+function isValidLastName(name){
+	var nameRegex = /^[-a-z]+$/;
+	if(nameRegex.test(name)){
+		return true;
+	} else {
+		return false;
+	}
+};
+
+function isValidUserName(username){
+	var nameRegex = /^[a-zA-Z0-9]+$/;
+	if(nameRegex.test(username)){
+		return true;
+	} else {
+		return false;
+	}
+};
+
+
+
+
