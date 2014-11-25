93bcefe48e2bd965ea7a12fe7149bd5ae920dc9b
aarondelaplane <aarondelaplane@icloud.com>
2014-11-24 20:22:17 -0500

    Merge error.

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,7 @@
 var resultsPlaceholder = document.getElementById('results'),
     playingCssClass = 'playing',
     audioObject = null;
-	
+
 
 var fetchTracks = function (albumId, callback) {
     $.ajax({
@@ -14,7 +14,7 @@ var fetchTracks = function (albumId, callback) {
 
 $(function(){
    var results = $('.cover')
-    results.on('click', function(e) {      
+    results.on('click', function(e) {
         var target = e.target;
         if (target !== null && target.classList.contains('cover')) {
             if (target.classList.contains(playingCssClass)) {
@@ -23,7 +23,7 @@ $(function(){
                 if (audioObject) {
                     audioObject.pause();
                 }
-                fetchTracks(target.getAttribute('data-album-id'), function(data) {            
+                fetchTracks(target.getAttribute('data-album-id'), function(data) {
                     audioObject = new Audio(data.tracks.items[0].preview_url);
                     audioObject.play();
                     target.classList.add(playingCssClass);
@@ -37,43 +37,43 @@ $(function(){
             }
         }
     });
-    $("#search-form").bind("submit", function(e) {
-        e.preventDefault();
-        $.ajax({
-            url: 'https://api.spotify.com/v1/search',
-            method: 'get',
-            data: {
-                q: $("#query").val(),
-                type: 'artist'
-                },
-            success: function (response) {
-                if (response["artists"]["items"].length === 0) {
-                    $("#search-error-msg").html("this artist does not exist")
-                    }
-                else {
-                    $("#search-form").unbind('submit').submit()
-                    }
-                }
-           });
-        });
-    $(".search-query").bind("submit", function(e) {
-        e.preventDefault();
-        $.ajax({
-            url: 'https://api.spotify.com/v1/search',
-            method: 'get',
-            data: {
-                q: $(".search-query").val(),
-                type: 'artist'
-                },
-            success: function (response) {
-                if (response["artists"]["items"].length === 0) {
-                    $(".search-error-msg").html("this artist does not exist")
-                    }
-                else {
-                    $(".search-query").unbind('submit').submit()
-                    }
-                }
-           });
-        });
+    // $("#search-form").bind("submit", function(e) {
+    //     e.preventDefault();
+    //     $.ajax({
+    //         url: 'https://api.spotify.com/v1/search',
+    //         method: 'get',
+    //         data: {
+    //             q: $("#query").val(),
+    //             type: 'artist'
+    //             },
+    //         success: function (response) {
+    //             if (response["artists"]["items"].length === 0) {
+    //                 $("#search-error-msg").html("this artist does not exist")
+    //                 }
+    //             else {
+    //                 $("#search-form").unbind('submit').submit()
+    //                 }
+    //             }
+    //        });
+    //     });
+    // $(".search-query").bind("submit", function(e) {
+    //     e.preventDefault();
+    //     $.ajax({
+    //         url: 'https://api.spotify.com/v1/search',
+    //         method: 'get',
+    //         data: {
+    //             q: $(".search-query").val(),
+    //             type: 'artist'
+    //             },
+    //         success: function (response) {
+    //             if (response["artists"]["items"].length === 0) {
+    //                 $(".search-error-msg").html("this artist does not exist")
+    //                 }
+    //             else {
+    //                 $(".search-query").unbind('submit').submit()
+    //                 }
+    //             }
+    //        });
+    //     });
 });
 
